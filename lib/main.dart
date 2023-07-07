25861b3d51c174c12260dd533a4ee32a1fbff79f
Enricko <enricko.putra028@gmail.com>
2023-07-07 19:13:06 +0700

    Firebase Config

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,15 @@ import 'package:absensi_app/home.dart';
 import 'package:absensi_app/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'edit_profile.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
